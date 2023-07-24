094344332ba2df99d87a43fea0568e8e0ac2f063
Timiemmy <faith.bolanle@yahoo.com>
2023-07-24 17:43:24 +0100

    Finished developing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# livescore
+Simple LiveScore Website Designed In Django
+
+This project make an api call to Apifootball.com to get already played football matches and their scores.
+
+Used bootstrap for the frontend.
+
--- scores/views.py
+++ scores/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+import requests
 
 def index(request):
-    return render(request, 'scores/index.html')
+    url = "" # Insert Url to make api call to
+    response = requests.get(url)
+    JsonResponse = response.json()
+    return render(request, 'scores/index.html', {'JsonResponse': JsonResponse})
